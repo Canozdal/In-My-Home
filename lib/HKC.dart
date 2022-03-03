c956fe3b3c3b8bcc9fc672f9fa5b0774682d5fe5
Hasankaanc <cakmakhasankaan@gmail.com>
2022-03-03 22:28:18 +0300

    Update HKC.dart

--- lib/HKC.dart
+++ lib/HKC.dart
@@ -1,30 +1,18 @@
 void main() {
-  List productList = [];
+  List users_product = [];
   var product1 = Product(productId: "milk",shelfLife: 10);
   var product2 = Product(productId: "milk", shelfLife: 4);
   var product3 = Product(productId: "milk", shelfLife: 7);
   var product4 = Product(productId: "pea",shelfLife: 9);
   var product5 = Product(productId: "pea",shelfLife: 5);
-  productList = idgenerator(productList, product1);
-  productList = idgenerator(productList, product2);
-  productList = idgenerator(productList, product3);
-  productList = idgenerator(productList, product4);
-  productList = idgenerator(productList, product5);
-  print(productList[0].productId);
-  print(productList[0].id);
-  print(productList[0].expirationDate);
-  print(productList[1].productId);
-  print(productList[1].id);
-  print(productList[1].expirationDate);
-  print(productList[2].productId);
-  print(productList[2].id);
-  print(productList[2].expirationDate);
-  print(productList[3].productId);
-  print(productList[3].id);
-  print(productList[3].expirationDate);
-  print(productList[4].productId);
-  print(productList[4].id);
-  print(productList[4].expirationDate);
+  users_product = product1.listupdater(users_product) ;
+  users_product = product2.listupdater(users_product) ;
+  users_product = product3.listupdater(users_product) ;
+  users_product = product4.listupdater(users_product) ;
+  users_product = product5.listupdater(users_product) ;
+  product2.listremover(users_product);
+  users_product = product2.listupdater(users_product) ;
+
 }
 
 class Product{
@@ -34,36 +22,38 @@ class Product{
   int id = 0;
   DateTime expirationDate = DateTime.now();
   
-}
-
-
-List idgenerator(List updatedList, var product){
-  product.id = 1;
+  List listupdater(List updatedList){
+  this.id = 1;
   int check = 0;
-  DateTime exp = product.expirationDate;
+  DateTime exp = this.expirationDate;
   if (updatedList.length > 0) {
     while (check == 0){
       check = 1;
       for (int i = 0; i < updatedList.length; i = i+1 ){
-        if (updatedList[i].productId == product.productId){
-          if (updatedList[i].id == product.id){
-            product.id ++;
+        if (updatedList[i].productId == this.productId){
+          if (updatedList[i].id == this.id){
+            this.id ++;
             check = 0;
           }
         }
       }
     }
   }
-  product.expirationDate = exp.add(Duration(days: product.shelfLife, hours: 0, minutes: 0));
-  updatedList.add(product);
+  this.expirationDate = exp.add(Duration(days: this.shelfLife, hours: 0, minutes: 0));
+  updatedList.add(this);
   return updatedList;
-}                     
+}   
+  void listremover(List updatedList){                         
+  updatedList.remove(this);
+  print('removed teacher => ${this.productId}');
+  }         
+}
+
+
+                  
                            
+                                                           
                            
-                           
-                           
-                           
-                           
-                           
+                          
                            
                            
