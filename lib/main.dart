380b8662579f06c38a47fef187d367f80042a792
Can Ozdal <canozdal@hotmail.com>
2022-03-17 19:05:32 +0200

    Merged code

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter1/screens/hesapscreen.dart';
 import 'package:flutter1/screens/kay%C4%B1tscreen.dart';
 import 'package:flutter1/screens/listscreen.dart';
 import 'package:flutter1/screens/login_screen.dart';
+import 'package:flutter1/screens/productekle.dart';
 import 'package:flutter1/screens/profile.dart';
 import 'package:flutter1/screens/tarama.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,12 +16,16 @@ FirebaseAuth auth = FirebaseAuth.instance;
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  List product_list = product_calistir();
 
 
-  runApp(MyApp());
+  runApp(MyApp(urun_list: product_list));
+
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({required this.urun_list});
+  List urun_list;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -36,7 +41,7 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/kayıt': (context) => KayitScreen(),
         '/kayıtolma': (context) => HesapScreen(),
-        '/list': (context) => ListScreen(),
+        '/list': (context) => ListScreen(urunlist: urun_list,),
         '/tarama': (context) => TaramaScreen(),
         '/profil' : (context) => ProfilePage(),
       },
--- lib/screens/listscreen.dart
+++ lib/screens/listscreen.dart
@@ -2,19 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter1/screens/constants.dart';
+import 'kutucukolustur.dart';
+import 'productekle.dart';
+import 'package:flutter1/main.dart';
 
 var butonrengi = Color(0x791074DE);
 var yazirengi = Color(0xF0FFFFFF);
 var butonrengi2= Color(0xFF083663);
 var butonrengi3= Color(0xFF487BEA);
 
+const List deneme = [];
+
+
 class ListScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
+  ListScreen({required this.urunlist});
+  List urunlist;
+  // @override
+  _LoginScreenState createState() => _LoginScreenState(urun_list: urunlist);
+
 }
 
 class _LoginScreenState extends State<ListScreen> {
   bool _rememberMe = false;
+  _LoginScreenState({required this.urun_list});
+  List urun_list;
+  // get urunlist => urunlist;
+  // get urunlist => Future.value(urunlist);
 
   Widget _buildaraTF() {
     return Column(
@@ -76,327 +89,33 @@ class _LoginScreenState extends State<ListScreen> {
       padding: EdgeInsets.symmetric(vertical: 1.0),
       width: double.infinity,
       child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Domates           25.12.2021             6 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun2TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Dondurma             28.12.2021             39 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun3TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Portakal             25.12.2021             6 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun4TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Yumurta             28.12.2021             13 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun5TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Ekmek             29.12.2021             4 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun6TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Peynir             28.12.2021             14 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun7TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Sucuk             21.12.2021             8 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun8TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Tereyağ             23.12.2021             15 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun9TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Sosis             23.12.2021             9 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun10TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Çikolata             28.12.2021             29 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun11TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Bisküvi             28.12.2021             159 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun12TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Patates             25.12.2021             6 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun13TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Tavuk             27.12.2021             5 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun14TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Ayran             29.12.2021             15 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildurun15TF() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 1.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Ev Bakım Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        color: butonrengi2,
-        child: Text(
-          'Süt             29.12.2021             7 Gün',
-          style: TextStyle(
-            color: yazirengi,
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
+          elevation: 5.0,
+          onPressed: () => print('Ev Bakım Button Pressed'),
+          padding: EdgeInsets.all(15.0),
+          color: butonrengi2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // use whichever suits your need
+            children: <Widget>[
+              Text(
+                urun_list[0].productname,
+                style: TextStyle(
+                  color: yazirengi,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'OpenSans',
+                ),
+              ),
+              Text(
+                urun_list[0].expirationDate.toString(),
+                style: TextStyle(
+                  color: yazirengi,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'OpenSans',
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
@@ -412,15 +131,15 @@ class _LoginScreenState extends State<ListScreen> {
           ),),
         toolbarHeight: 80.0,
         title: Text(
-              '             Gıda',
-              style: TextStyle(
-                color: yazirengi,
-                fontFamily: 'OpenSans',
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          '             Gıda',
+          style: TextStyle(
+            color: yazirengi,
+            fontFamily: 'OpenSans',
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+      ),
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: GestureDetector(
@@ -458,21 +177,26 @@ class _LoginScreenState extends State<ListScreen> {
                     children: <Widget>[
                       _buildaraTF(),
                       _buildheadTF(),
-                      _buildurun1TF(),
-                      _buildurun2TF(),
-                      _buildurun3TF(),
-                      _buildurun4TF(),
-                      _buildurun5TF(),
-                      _buildurun6TF(),
-                      _buildurun7TF(),
-                      _buildurun8TF(),
-                      _buildurun9TF(),
-                      _buildurun10TF(),
-                      _buildurun11TF(),
-                      _buildurun12TF(),
-                      _buildurun13TF(),
-                      _buildurun14TF(),
-                      _buildurun15TF(),
+                      for ( var i in urun_list )
+                        SignInButton(
+                          text: i.productname,
+                          text1: i.expirationDate.toString(),
+                          onPressed: () {},
+                        ),
+                      // _buildurun2TF(),
+                      // _buildurun3TF(),
+                      // _buildurun4TF(),
+                      // _buildurun5TF(),
+                      // _buildurun6TF(),
+                      // _buildurun7TF(),
+                      // _buildurun8TF(),
+                      // _buildurun9TF(),
+                      // _buildurun10TF(),
+                      // _buildurun11TF(),
+                      // _buildurun12TF(),
+                      // _buildurun13TF(),
+                      // _buildurun14TF(),
+                      // _buildurun15TF(),
                     ],
                   ),
                 ),
